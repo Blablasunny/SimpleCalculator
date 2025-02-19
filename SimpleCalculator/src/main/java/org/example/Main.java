b760d28910c9a56a5bf109c1429152c771fb95c9
Anna-web <Ann.flora.2004@mail.ru>
2025-02-19 12:14:53 +0300

    Multiply-divide

--- SimpleCalculator/src/main/java/org/example/Main.java
+++ SimpleCalculator/src/main/java/org/example/Main.java
@@ -4,12 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    public float add(float a,float b) {
-        return a+b;
-    }
-    public float subtract(float a,float b) {
-        return a - b;
-    }
 
     public float multiply(float a,float b) {
         return a * b;
