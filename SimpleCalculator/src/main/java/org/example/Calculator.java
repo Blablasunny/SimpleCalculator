3b3d9d5332f5da28c17382709ae7d327ba523cf0
Anna-web <Ann.flora.2004@mail.ru>
2025-02-19 12:17:30 +0300

    Multiply-divide

--- SimpleCalculator/src/main/java/org/example/Calculator.java
+++ SimpleCalculator/src/main/java/org/example/Calculator.java
@@ -1,5 +1,10 @@
 package org.example;
 
 public class Calculator {
-
+    public float add(float a,float b) {
+        return a+b;
+    }
+    public float subtract(float a,float b) {
+        return a - b;
+    }
 }
